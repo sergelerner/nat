3e5050b02876f96f46e2557cbfb16ba24152d34b
Sergey Lerner <sergel@netcraft.co.il>
2013-12-14 18:39:26 +0200

    build6

--- app/scripts/controllers/footerCtrl.js
+++ app/scripts/controllers/footerCtrl.js
@@ -5,27 +5,27 @@ angular.module('nat').controller('footerCtrl', function ($scope) {
 	$scope.socials = [
 		{
 			title: "Facebook",
-			image: "../images/social/facebook.png",
+			image: "images/social/facebook.png",
 			link : ""
 		},
 		{
 			title: "Linkedin",
-			image: "../images/social/linkedin.png",
+			image: "images/social/linkedin.png",
 			link : ""
 		},
 		{
 			title: "Instagram",
-			image: "../images/social/instagram.png",
+			image: "images/social/instagram.png",
 			link : ""
 		},
 		{
 			title: "Pinterest",
-			image: "../images/social/pinterest.png",
+			image: "images/social/pinterest.png",
 			link : ""
 		},
 		{
 			title: "Mail",
-			image: "../images/social/mail.png",
+			image: "images/social/mail.png",
 			link : ""
 		}
 	];
