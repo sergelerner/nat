36588c03e869455dd8b56745dbaaa4b9dde900dd
Sergey Lerner <sergel@netcraft.co.il>
2013-12-14 19:02:17 +0200

    build10

---
+++ dist/scripts/controllers/categoryCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('nat').controller('categoryCtrl', function ($scope, $routeParams, workServ) {
+
+	$scope.works = workServ.getCategories($routeParams.categoryId);	
+
+	console.log('categoryCtrl');
+	$(window).scrollTop(500,200);
+	
+});
